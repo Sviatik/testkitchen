0b20f3d500183c807c87d8e1638b9cc97fa9f032
Святослав Стецяк <s.stetsyak@gmail.com>
2017-02-21 15:31:02 +0200

    update

--- test/integration/default/roles/common/spec/common_spec.rb
+++
@@ -1,5 +0,0 @@
-require '/tmp/kitchen/roles/common/spec/spec_helper.rb'
-
-describe package( 'curl' ) do
-    it { should be_installed }
-end
--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'ansible-$role::default' do
+#describe 'ansible-$role::default' do
 
   ## serverspec tests go here
   #
@@ -8,13 +8,42 @@ describe 'ansible-$role::default' do
   #   it { should be_installed.by('yum') }
   # end
 
-   describe user('root') do
-     it { should exist }
-   end
 
-   describe package('curl') do
-     it { should be_installed }
-   end
+describe "Check default conf file" do
+  describe file('/etc/nginx/nginx.conf') do
+    it { should exist }
+  end
+end
 
 
+describe "Check current conf file" do
+  describe file('/etc/nginx/sites-enabled/default') do
+    it { should exist }
+  end
+end
+
+
+describe "Check packages" do
+  describe package('curl') do
+    it { should be_installed }
+  end
+end
+
+describe "Check nginx package" do
+  describe package('nginx') do
+    it { should be_installed }
+  end
+end
+
+describe 'check nginx enabled' do
+  describe service('nginx') do
+    it { should be_enabled }
+  end
+end
+
+
+describe 'check port' do
+  describe port(80) do
+    it { should be_listening.with('tcp') }
+  end
 end
